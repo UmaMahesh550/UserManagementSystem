bc04a9e29d31f9acf0fb1e64feb46755f4483d96
Gorsa Uma Mahesh <gorsaumamahesh@gmail.com>
2022-05-22 11:37:47 +0530

    Added Listuser, Adduser and Deleteuser components

--- src/app/user.ts
+++ src/app/user.ts
@@ -2,7 +2,7 @@ export class User{
     id?: number;
     firstname?:string;
     lastname?:string;
-    roles?:String;
+    role?:String;
     age?:number;
     email?:string;
     password?:string;
@@ -12,7 +12,7 @@ export interface IUser{
     id : number,
     firstname : string,
     lastname : string,
-    roles : string,
+    role : string,
     age : number,
     email : string,
     password : string,
